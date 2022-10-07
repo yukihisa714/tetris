ba28f76da96bef3cff98a705e6a9f3db7f1a5d30
YukihisaHida <yukihisa714@gmail.com>
2022-10-07 22:07:31 +0900

    init

---
+++ main.js
@@ -0,0 +1,118 @@
+const BLOCK_SIZE = 20;
+const BLOCKS_COL = 10;
+const BLOCKS_ROW = 20;
+const FIELD_WIDtH = BLOCK_SIZE * BLOCKS_COL;
+const FIELD_HEIGHT = BLOCK_SIZE * BLOCKS_ROW;
+
+const can = document.createElement("canvas");
+const con = can.getContext("2d");
+can.width = FIELD_WIDtH;
+can.height = FIELD_HEIGHT;
+// can.style.background = "lightgray";
+document.body.appendChild(can);
+
+
+let field = [];
+for (let row = 0; row < BLOCKS_ROW; row++) {
+    field[row] = [];
+    for (let col = 0; col < BLOCKS_COL; col++) {
+        field[row][col] = 0;
+    }
+}
+
+let frame = 0;
+
+const keyOpe = {
+    ArrowUp: false,
+    ArrowDown: false,
+    ArrowLeft: false,
+    ArrowRight: false,
+};
+
+document.onkeydown = (e) => {
+    // console.log(e.key);
+    keyOpe[e.key] = true;
+
+    switch (e.key) {
+        case " ":
+            mino.dropMino();
+            break;
+
+        default:
+            break;
+    }
+};
+document.onkeyup = (e) => {
+    keyOpe[e.key] = false;
+}
+
+const GAME_STATES = {
+    beforeGame: 0,
+    inGame: 1,
+    afterGame: 2,
+};
+
+let nowGameState = GAME_STATES.beforeGame;
+
+function removeLine() {
+    for (let y = BLOCKS_ROW - 1; y >= 0; y--) {
+        let p = 1;
+        for (const x of field[y]) {
+            p *= x;
+        }
+        if (!p) continue;
+        for (let py = y; py >= 1; py--) {
+            field[py] = field[py - 1];
+        }
+        field[0] = Array(10).fill(0);
+        y++;
+    }
+}
+
+
+function drawOneBlock(x, y, fillColor, strokeColor) {
+    con.fillStyle = fillColor;
+    con.fillRect(x * BLOCK_SIZE, y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+    con.strokeStyle = strokeColor;
+    con.strokeRect(x * BLOCK_SIZE, y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+}
+
+function drawAllBlocks() {
+    for (let y = 0; y < BLOCKS_ROW; y++) {
+        for (let x = 0; x < BLOCKS_COL; x++) {
+            drawOneBlock(x, y, colors[field[y][x]], "dimgray");
+        }
+    }
+}
+
+function mainLoop() {
+    frame++;
+    con.clearRect(0, 0, FIELD_WIDtH, FIELD_HEIGHT);
+    drawAllBlocks();
+
+    if (!mino.check(0, 0, 0)) {
+        nowGameState = GAME_STATES.afterGame;
+    }
+
+    if (frame % 60 === 0) {
+        mino.moveMino(0, 1, 0);
+        removeLine();
+    }
+
+    predictMino.x = mino.x;
+    predictMino.y = mino.y;
+    predictMino.rotate = mino.rotate;
+    predictMino.dropMino();
+
+    predictMino.draw();
+    mino.draw();
+    mino.update();
+
+    con.fillStyle = "white"
+    con.fillText(keyOpe.ArrowUp, 10, 10);
+    con.fillText(keyOpe.ArrowDown, 10, 20);
+    con.fillText(keyOpe.ArrowLeft, 10, 30);
+    con.fillText(keyOpe.ArrowRight, 10, 40);
+}
+
+setInterval(mainLoop, 1000 / 60);
---
+++ mino.js
@@ -0,0 +1,299 @@
+const colors = ["darkgray", "skyblue", "yellow", "green", "red", "blue", "orange", "pink"];
+
+const MINO_SHAPE = [
+    // none
+    undefined,
+    // I
+    [
+        [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [1, 1, 1, 1],
+            [0, 0, 0, 0],
+        ], [
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+        ], [
+            [0, 0, 0, 0],
+            [1, 1, 1, 1],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+        ], [
+            [0, 0, 1, 0],
+            [0, 0, 1, 0],
+            [0, 0, 1, 0],
+            [0, 0, 1, 0],
+        ],
+    ],
+    // O
+    [
+        [
+            [0, 0, 0, 0],
+            [0, 2, 2, 0],
+            [0, 2, 2, 0],
+            [0, 0, 0, 0],
+        ], [
+            [0, 0, 0, 0],
+            [0, 2, 2, 0],
+            [0, 2, 2, 0],
+            [0, 0, 0, 0],
+        ], [
+            [0, 0, 0, 0],
+            [0, 2, 2, 0],
+            [0, 2, 2, 0],
+            [0, 0, 0, 0],
+        ], [
+            [0, 0, 0, 0],
+            [0, 2, 2, 0],
+            [0, 2, 2, 0],
+            [0, 0, 0, 0],
+        ],
+    ],
+    // S
+    [
+        [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 3, 3, 0],
+            [3, 3, 0, 0],
+        ], [
+            [0, 0, 0, 0],
+            [3, 0, 0, 0],
+            [3, 3, 0, 0],
+            [0, 3, 0, 0],
+        ], [
+            [0, 0, 0, 0],
+            [0, 3, 3, 0],
+            [3, 3, 0, 0],
+            [0, 0, 0, 0],
+        ], [
+            [0, 0, 0, 0],
+            [0, 3, 0, 0],
+            [0, 3, 3, 0],
+            [0, 0, 3, 0],
+        ],
+    ],
+    // Z
+    [
+        [
+            [0, 0, 0, 0],
+            [4, 4, 0, 0],
+            [0, 4, 4, 0],
+            [0, 0, 0, 0],
+        ], [
+            [0, 0, 0, 0],
+            [0, 0, 4, 0],
+            [0, 4, 4, 0],
+            [0, 4, 0, 0],
+        ], [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [4, 4, 0, 0],
+            [0, 4, 4, 0],
+        ], [
+            [0, 0, 0, 0],
+            [0, 4, 0, 0],
+            [4, 4, 0, 0],
+            [4, 0, 0, 0],
+        ],
+    ],
+    // J
+    [
+        [
+            [0, 0, 0, 0],
+            [5, 0, 0, 0],
+            [5, 5, 5, 0],
+            [0, 0, 0, 0],
+        ], [
+            [0, 0, 0, 0],
+            [0, 5, 5, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
+        ], [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [5, 5, 5, 0],
+            [0, 0, 5, 0],
+        ], [
+            [0, 0, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
+            [5, 5, 0, 0],
+        ],
+    ],
+    // L
+    [
+        [
+            [0, 0, 0, 0],
+            [0, 0, 6, 0],
+            [6, 6, 6, 0],
+            [0, 0, 0, 0],
+        ], [
+            [0, 0, 0, 0],
+            [0, 6, 0, 0],
+            [0, 6, 0, 0],
+            [0, 6, 6, 0],
+        ], [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [6, 6, 6, 0],
+            [6, 0, 0, 0],
+        ], [
+            [0, 0, 0, 0],
+            [6, 6, 0, 0],
+            [0, 6, 0, 0],
+            [0, 6, 0, 0],
+        ],
+    ],
+    // T
+    [
+        [
+            [0, 0, 0, 0],
+            [0, 7, 0, 0],
+            [7, 7, 7, 0],
+            [0, 0, 0, 0],
+        ], [
+            [0, 0, 0, 0],
+            [0, 7, 0, 0],
+            [0, 7, 7, 0],
+            [0, 7, 0, 0],
+        ], [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [7, 7, 7, 0],
+            [0, 7, 0, 0],
+        ], [
+            [0, 0, 0, 0],
+            [0, 7, 0, 0],
+            [7, 7, 0, 0],
+            [0, 7, 0, 0],
+        ],
+    ],
+];
+
+class Mino {
+    constructor(x, y, type, reality) {
+        this.x = x;
+        this.y = y;
+        this.type = type;
+        this.rotate = 0;
+        this.maxStrength = 60 * 1;
+        this.strength = this.maxStrength;
+        this.deathStart = false;
+
+        this.fillColor = reality ? colors[this.type] : "silver";
+        this.strokeColor = reality ? "black" : "lightgray";
+
+        this.keyCount = {
+            ArrowUp: 0,
+            ArrowDown: 0,
+            ArrowLeft: 0,
+            ArrowRight: 0,
+        };
+    }
+    // ミノをフィールドに固定する
+    setMino() {
+        for (let y = 0; y < 4; y++) {
+            for (let x = 0; x < 4; x++) {
+                const p = MINO_SHAPE[this.type][this.rotate][y][x];
+                if (p) {
+                    field[this.y + y][this.x + x] = p;
+                }
+            }
+        }
+    }
+    // ミノが動く先に障害物があるかどうか判断する
+    /**
+     * 
+     * @param {Number} mvoeX 左右移動
+     * @param {Number} moveY 上下移動
+     * @param {Number} rotate 右回転
+     * @returns True or False
+     */
+    check(mvoeX, moveY, rotate) {
+        for (let y = 0; y < 4; y++) {
+            for (let x = 0; x < 4; x++) {
+                if (MINO_SHAPE[this.type][(this.rotate + rotate) % 4][y][x]) {
+                    if (this.x + mvoeX + x < 0 || BLOCKS_COL <= this.x + mvoeX + x) return false;
+                    if (this.y + moveY + y < 0 || BLOCKS_ROW <= this.y + moveY + y) return false;
+                    if (field[this.y + moveY + y][this.x + mvoeX + x]) return false;
+                }
+            }
+        }
+        return true;
+    }
+    // ミノを動かす
+    /**
+     * 
+     * @param {Number} mvoeX 左右移動
+     * @param {Number} moveY 上下移動
+     * @param {Number} rotate 右回転
+     * @returns 
+     */
+    moveMino(mvoeX, moveY, rotate) {
+        if (!this.check(mvoeX, moveY, rotate)) {
+            return;
+        }
+        this.x += mvoeX;
+        this.y += moveY;
+        this.rotate += rotate;
+        this.rotate = this.rotate % 4;
+    }
+    // ミノを下まで一気に落とす
+    dropMino() {
+        while (this.check(0, 1, 0)) {
+            this.moveMino(0, 1, 0);
+        }
+        removeLine();
+    }
+    draw() {
+        for (let y = 0; y < 4; y++) {
+            for (let x = 0; x < 4; x++) {
+                const p = MINO_SHAPE[this.type][this.rotate][y][x];
+                if (p) {
+                    drawOneBlock(this.x + x, this.y + y, this.fillColor, this.strokeColor);
+                }
+            }
+        }
+    }
+    update() {
+
+        this.keyCount.ArrowUp = (keyOpe.ArrowUp) ? this.keyCount.ArrowUp + 1 : 0;
+        this.keyCount.ArrowDown = (keyOpe.ArrowDown) ? this.keyCount.ArrowDown + 1 : 0;
+        this.keyCount.ArrowLeft = (keyOpe.ArrowLeft) ? this.keyCount.ArrowLeft + 1 : 0;
+        this.keyCount.ArrowRight = (keyOpe.ArrowRight) ? this.keyCount.ArrowRight + 1 : 0;
+
+        if (this.keyCount.ArrowUp % 15 === 1) this.moveMino(0, 0, 1);
+        if (this.keyCount.ArrowDown % 10 === 1) this.moveMino(0, 1, 0);
+        if (this.keyCount.ArrowLeft % 10 === 1) this.moveMino(-1, 0, 0);
+        if (this.keyCount.ArrowRight % 10 === 1) this.moveMino(1, 0, 0);
+
+        if (!this.check(0, 1, 0)) {
+            if (this.deathStart) {
+                this.strength--;
+                if (!this.strength) {
+                    this.setMino();
+                    makeMino();
+                    removeLine();
+                    // mino = new Mino(3, 0, 1);
+                }
+            }
+            else {
+                this.deathStart = true;
+            }
+        }
+    }
+}
+
+let mino;
+let predictMino;
+makeMino();
+
+function makeMino() {
+    const type = Math.floor(Math.random() * 7) + 1;
+    mino = new Mino(3, 0, type, true);
+    predictMino = new Mino(3, 0, type, false);
+}
+// mino.setMino();
